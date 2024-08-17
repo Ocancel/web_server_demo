083253fb11d3d584a63b589a1346fc39065b5d47
Jiaqi Gao <jiaqi.gao.dev@gmail.com>
2024-08-17 23:58:30 +0800

    Init commit.

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::{fs, io::{Write, BufRead, BufReader}, net::{TcpListener, TcpStream}};
+use web_server_demo::ThreadPool;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(5);
+    for stream in listener.incoming().take(2) {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let reader = BufReader::new(&mut stream);
+    let request_line = reader
+        .lines()
+        .next().unwrap().unwrap();
+    let (status_line, file_name) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+    let contents = fs::read_to_string(file_name).unwrap();
+    let length = contents.len();
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap()
+}
+
